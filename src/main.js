29e4799ee0723ec976fc91588bc27a9022caed2c
Estefany <estefany.espinoza.h@gmail.com>
2022-01-12 17:42:35 -0300

    instalacion primevue

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Primevue from 'primevue/config';
+const app = createApp(App);
+
+app.use(Primevue)
 
 createApp(App).use(store).use(router).mount('#app')
